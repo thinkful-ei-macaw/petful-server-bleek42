0af70dee79344487b073031ffaa4243b026d68b2
bleek42 <brandonleek42@gmail.com>
2020-06-06 21:59:34 -0400

    final commit before deploy

---
+++ config.js
@@ -0,0 +1,6 @@
+/* eslint-disable strict */
+module.exports = {
+  CLIENT_ORIGIN: process.env.CLIENT_ORIGIN || 'http://localhost:3000',
+  PORT: process.env.PORT || 8000,
+  NODE_ENV: process.env.NODE_ENV || 'development',
+};
--- store.js
+++ store.js
@@ -1,19 +1,18 @@
+/* eslint-disable strict */
 module.exports = {
-  people: [
-    'Randy Lahey',
-    'Trevor Cory',
-    'Jim Lahey'
-  ],
+  people: ['Randy Lahey', 'Trevor Cory', 'Jim Lahey'],
 
   dogs: [
     {
       age: 3,
       breed: 'Golden Retriever',
-      description: 'A smiling golden-brown golden retreiver listening to music.',
+      description:
+        'A smiling golden-brown golden retreiver listening to music.',
       gender: 'Male',
-      imageURL: 'https://images.pexels.com/photos/33053/dog-young-dog-small-dog-maltese.jpg?auto=compress&cs=tinysrgb&dpr=1&w=500',
+      imageURL:
+        'https://images.pexels.com/photos/33053/dog-young-dog-small-dog-maltese.jpg?auto=compress&cs=tinysrgb&dpr=1&w=500',
       name: 'Zim',
-      story: 'Owner Passed away'
+      story: 'Owner Passed away',
     },
 
     {
@@ -21,19 +20,21 @@ module.exports = {
       breed: 'Akita',
       description: 'A smiling golden-brown dog',
       gender: 'Female',
-      imageURL: 'https://images.pexels.com/photos/1254140/pexels-photo-1254140.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
+      imageURL:
+        'https://images.pexels.com/photos/1254140/pexels-photo-1254140.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
       name: 'Fran',
-      story: 'Found downtown'
+      story: 'Found downtown',
     },
 
     {
-      imageURL: 'https://images.pexels.com/photos/160846/french-bulldog-summer-smile-joy-160846.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
+      imageURL:
+        'https://images.pexels.com/photos/160846/french-bulldog-summer-smile-joy-160846.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
       description: 'Black pug.',
       name: 'Bilbo',
       gender: 'Male',
       age: 3,
       breed: 'Pug',
-      story: 'Owner Passed away'
+      story: 'Owner Passed away',
     },
 
     {
@@ -41,9 +42,10 @@ module.exports = {
       breed: 'Collie',
       description: 'Border collie.',
       gender: 'Male',
-      imageURL: 'https://images.pexels.com/photos/733416/pexels-photo-733416.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
+      imageURL:
+        'https://images.pexels.com/photos/733416/pexels-photo-733416.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
       name: 'Philip',
-      story: 'Owner Passed away'
+      story: 'Owner Passed away',
     },
 
     {
@@ -51,29 +53,34 @@ module.exports = {
       breed: 'Pug',
       description: 'pug.',
       gender: 'Male',
-      imageURL: 'https://images.pexels.com/photos/374906/pexels-photo-374906.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
+      imageURL:
+        'https://images.pexels.com/photos/374906/pexels-photo-374906.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
       name: 'Doogie',
-      story: 'Owner Passed away'
+      story: 'Owner Passed away',
     },
 
     {
-      imageURL: 'https://images.pexels.com/photos/551628/pexels-photo-551628.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
-      description: 'A smiling golden-brown golden retreiver listening to music.',
+      imageURL:
+        'https://images.pexels.com/photos/551628/pexels-photo-551628.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
+      description:
+        'A smiling golden-brown golden retreiver listening to music.',
       name: 'Zeke',
       gender: 'Male',
       age: 3,
       breed: 'Golden Retriever',
-      story: 'Owner Passed away'
+      story: 'Owner Passed away',
     },
 
     {
       age: 3,
       breed: 'Golden Retriever',
-      description: 'A smiling golden-brown golden retreiver listening to music.',
+      description:
+        'A smiling golden-brown golden retreiver listening to music.',
       gender: 'Male',
-      imageURL: 'https://images.pexels.com/photos/1490908/pexels-photo-1490908.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500',
+      imageURL:
+        'https://images.pexels.com/photos/1490908/pexels-photo-1490908.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500',
       name: 'Sam',
-      story: 'Owner Passed away'
+      story: 'Owner Passed away',
     },
 
     {
@@ -81,9 +88,10 @@ module.exports = {
       breed: 'Fluffy Dog',
       description: 'Australian sheppard',
       gender: 'Female',
-      imageURL: 'https://images.pexels.com/photos/1322182/pexels-photo-1322182.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500',
+      imageURL:
+        'https://images.pexels.com/photos/1322182/pexels-photo-1322182.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500',
       name: 'Smelly',
-      story: 'Owner Passed away'
+      story: 'Owner Passed away',
     },
 
     {
@@ -91,9 +99,10 @@ module.exports = {
       breed: 'Terrier',
       description: 'terrier',
       gender: 'Male',
-      imageURL: 'https://images.pexels.com/photos/257540/pexels-photo-257540.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
+      imageURL:
+        'https://images.pexels.com/photos/257540/pexels-photo-257540.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
       name: 'Tiny',
-      story: 'Found downtown'
+      story: 'Found downtown',
     },
 
     {
@@ -101,9 +110,10 @@ module.exports = {
       breed: 'Golden Retriever',
       description: 'A smiling golden-brown golden retreiver',
       gender: 'Male',
-      imageURL: 'https://images.pexels.com/photos/92380/pexels-photo-92380.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500',
+      imageURL:
+        'https://images.pexels.com/photos/92380/pexels-photo-92380.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500',
       name: 'Tom',
-      story: 'Owner surrrendered dog'
+      story: 'Owner surrrendered dog',
     },
 
     {
@@ -111,10 +121,11 @@ module.exports = {
       breed: 'Boxer',
       description: 'boxer dog',
       gender: 'Male',
-      imageURL: 'https://images.pexels.com/photos/1629781/pexels-photo-1629781.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
+      imageURL:
+        'https://images.pexels.com/photos/1629781/pexels-photo-1629781.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
       name: 'Tim',
-      story: 'Found in neighborhood'
-    }
+      story: 'Found in neighborhood',
+    },
   ],
 
   cats: [
@@ -123,9 +134,10 @@ module.exports = {
       breed: 'Bengal',
       description: 'Orange bengal cat with black stripes lounging on concrete.',
       gender: 'Female',
-      imageURL:'https://images.pexels.com/photos/104827/cat-pet-animal-domestic-104827.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500', 
+      imageURL:
+        'https://images.pexels.com/photos/104827/cat-pet-animal-domestic-104827.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
       name: 'Steve French',
-      story: 'Thrown on the street'
+      story: 'Thrown on the street',
     },
 
     {
@@ -133,9 +145,10 @@ module.exports = {
       breed: 'white cat',
       description: 'White cat',
       gender: 'Female',
-      imageURL:'https://images.pexels.com/photos/617278/pexels-photo-617278.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500', 
+      imageURL:
+        'https://images.pexels.com/photos/617278/pexels-photo-617278.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
       name: 'Snapjacks',
-      story: 'Thrown on the street'
+      story: 'Thrown on the street',
     },
 
     {
@@ -143,9 +156,10 @@ module.exports = {
       breed: 'Bengal',
       description: 'Grey cat',
       gender: 'Female',
-      imageURL:'https://images.pexels.com/photos/730896/pexels-photo-730896.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500', 
+      imageURL:
+        'https://images.pexels.com/photos/730896/pexels-photo-730896.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
       name: 'Grumpy-Face',
-      story: 'Thrown on the street'
+      story: 'Thrown on the street',
     },
 
     {
@@ -153,18 +167,20 @@ module.exports = {
       breed: 'Bengal',
       description: 'Orange cat',
       gender: 'Female',
-      imageURL:'https://images.pexels.com/photos/57416/cat-sweet-kitty-animals-57416.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500', 
+      imageURL:
+        'https://images.pexels.com/photos/57416/cat-sweet-kitty-animals-57416.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
       name: 'Garry Lazereyes',
-      story: 'Thrown on the street'
+      story: 'Thrown on the street',
     },
     {
       age: 2,
       breed: 'Bengal',
       description: 'Grey cat',
       gender: 'Male',
-      imageURL:'https://images.pexels.com/photos/126407/pexels-photo-126407.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500', 
+      imageURL:
+        'https://images.pexels.com/photos/126407/pexels-photo-126407.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
       name: 'Neapolitan Charlie',
-      story: 'Thrown on the street'
+      story: 'Thrown on the street',
     },
 
     {
@@ -172,9 +188,10 @@ module.exports = {
       breed: 'Tabby',
       description: 'Orange bengal cat with black stripes lounging on concrete.',
       gender: 'Female',
-      imageURL:'https://images.pexels.com/photos/774731/pexels-photo-774731.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500', 
+      imageURL:
+        'https://images.pexels.com/photos/774731/pexels-photo-774731.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
       name: 'Daisy',
-      story: 'Thrown on the street'
+      story: 'Thrown on the street',
     },
 
     {
@@ -182,9 +199,10 @@ module.exports = {
       breed: 'Bengal',
       description: 'Brown kitty',
       gender: 'Female',
-      imageURL:'https://images.pexels.com/photos/1543793/pexels-photo-1543793.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500', 
+      imageURL:
+        'https://images.pexels.com/photos/1543793/pexels-photo-1543793.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
       name: 'Stinkster',
-      story: 'Thrown on the street'
+      story: 'Thrown on the street',
     },
 
     {
@@ -192,9 +210,10 @@ module.exports = {
       breed: 'Bengal',
       description: 'kitty',
       gender: 'Female',
-      imageURL:'https://images.pexels.com/photos/1276553/pexels-photo-1276553.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500', 
+      imageURL:
+        'https://images.pexels.com/photos/1276553/pexels-photo-1276553.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
       name: 'Ner-Ner',
-      story: 'Thrown on the street'
+      story: 'Thrown on the street',
     },
 
     {
@@ -202,9 +221,10 @@ module.exports = {
       breed: 'Tabby',
       description: 'grey cat',
       gender: 'Male',
-      imageURL:'https://images.pexels.com/photos/54632/cat-animal-eyes-grey-54632.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500', 
+      imageURL:
+        'https://images.pexels.com/photos/54632/cat-animal-eyes-grey-54632.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500',
       name: 'Furrnando Valenzuela',
-      story: 'Thrown on the street'
+      story: 'Thrown on the street',
     },
 
     {
@@ -212,7 +232,8 @@ module.exports = {
       breed: 'Tabby',
       description: 'grey cat',
       gender: 'Male',
-      imageURL:'https://images.pexels.com/photos/923360/pexels-photo-923360.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500', 
+      imageURL:
+        'https://images.pexels.com/photos/923360/pexels-photo-923360.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
       name: 'Spike',
       story: 'Thrown on the street',
     },
@@ -222,9 +243,10 @@ module.exports = {
       breed: 'Tabby',
       description: 'boxer dog',
       gender: 'Male',
-      imageURL: 'https://images.pexels.com/photos/1472999/pexels-photo-1472999.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
+      imageURL:
+        'https://images.pexels.com/photos/1472999/pexels-photo-1472999.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
       name: 'Vince the Pince',
-      story: 'Found in neighborhood'
-    }
-  ]
-}
+      story: 'Found in neighborhood',
+    },
+  ],
+};
