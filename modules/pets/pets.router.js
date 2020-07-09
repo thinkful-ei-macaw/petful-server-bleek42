3e101ef13fc3993f387597e50b6a9fb611b615e5
bleek42 <brandonleek42@gmail.com>
2020-07-08 21:03:06 -0400

    consolidated get pets endpoint to one

--- modules/pets/pets.router.js
+++ modules/pets/pets.router.js
@@ -5,28 +5,17 @@ const jsonParser = require('body-parser').json();
 
 const Pets = require('./pets.service');
 const People = require('../people/people.service');
-const e = require('express');
 
 const petsRouter = express.Router();
 
-petsRouter.route('/dogs').get((req, res) => {
+petsRouter.route('/next').get((req, res) => {
   // if (req.body !== 'dog') {
   //   res.status(400).json({
   //     message: 'request body must have value of dog!',
   //   });
   // }
-  const { nextDog } = Pets.get();
-  return res.status(200).json({ nextDog });
-});
-
-petsRouter.get('/cats', (req, res) => {
-  // if (req.body !== 'cat') {
-  //   res.status(400).json({
-  //     message: 'request body must have value of cat!',
-  //   });
-  // }
-  const { nextCat } = Pets.get();
-  return res.status(200).json({ nextCat });
+  const currentPets = Pets.get();
+  return res.status(200).json({ currentPets });
 });
 
 petsRouter.get('/all', (req, res) => {
